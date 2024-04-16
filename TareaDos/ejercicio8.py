be980e46ed187c1ecdf6da8fe30bdac497d97cec
ewalshpcr <97931085+ewalshpcr@users.noreply.github.com>
2024-04-16 08:42:51 -0600

    Update ejercicio8.py, mejora de del logaritmo

--- TareaDos/ejercicio8.py
+++ TareaDos/ejercicio8.py
@@ -3,14 +3,17 @@ numero = int(input("Digite un número entero positivo, sin decimales: "))
 def esNumeroPrimo(numeroAConsultar):
     contador = 0
 
-    for entero in range(1, (numeroAConsultar + 1)):
-        #print(numeroAConsultar, entero)
+    for entero in range(2, numeroAConsultar):
         if numeroAConsultar % entero == 0:
             contador += 1
 
-    if contador == 2:
+    #Cumpliendo la regla que sean divisibles solo por ellos mismo y 1
+    if (numeroAConsultar % 1 == 0) and (numeroAConsultar % numeroAConsultar == 0) and contador == 0:
         print(f"El número {numeroAConsultar} es primo")
     else:
         print(f"El número {numeroAConsultar} No es primo")
 
 esNumeroPrimo(numero)
+
+#for x in range(2, 101):
+#    esNumeroPrimo(x)
